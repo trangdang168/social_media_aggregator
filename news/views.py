34b4ce4e95a7c4d8d846bf4402e17653a99adab6
Trang Dang <huyentrangdang160801@gmail.com>
2021-06-16 15:20:23 +0700

    finish styling accounts and news

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 
-#from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 #from django.contrib.auth.mixins import LoginRequiredMixin
 
 import requests
